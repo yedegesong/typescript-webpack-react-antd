9bd287605d3ce47343d3c1af7602ea03dc742ebe
yedegesong <277527478@qq.com>
2016-05-10 16:32:02 +0800

    删除重新发送

--- www/README.md
+++
@@ -1,6 +0,0 @@
-# react-frozenUI
-一点点编写react-frozenUI 持续完善中
-
-### 在线预览地址
-http://yedegesong.github.io/react-frozenUI/publish
-
--- www/filepath.js
+++
@@ -1,8 +0,0 @@
-/**
- * 引入文件入口 - 模块
- * 文件主入口目录 键key表示 编译出需要引入的JS文件名 值value 表示 需要待编译的tsx文件
- * @type {{index: string, details: string}}
- */
-module.exports = {
-  IndexContainer:"IndexContainer"
-}
