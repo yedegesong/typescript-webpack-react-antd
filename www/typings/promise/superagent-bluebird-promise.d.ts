caff7d8082003d949515cf4d5db0ba9834e1a580
yedegesong <277527478@qq.com>
2016-06-01 13:39:33 +0800

    全部删除

--- www/typings/promise/superagent-bluebird-promise.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 申明拖上传
- */
-declare var Request: any;
-
-declare module "superagent-bluebird-promise" {
-	export = Request
-}
--- www/typings/rc-upload/rc-upload.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 申明拖上传
- */
-declare var Upload: any;
-
-declare module "rc-upload" {
-	export = Upload
-}
-
